0b539b3a1f8463f2d0cefaeb008d2e9a2daa7122
Vi Hoàng Anh Tú <vihoanganhtu25042005@gmail.com>
2026-06-18 13:27:29 +0700

    fix index

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('copy').tap(args => {
+      if (args[0]?.patterns?.[0]?.globOptions?.ignore) {
+        args[0].patterns[0].globOptions.ignore.push('**/index.html');
+      }
+      return args;
+    });
+  }
 })
